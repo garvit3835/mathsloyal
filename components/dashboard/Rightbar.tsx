535c69efbbc7c5fc3ccbbad06060902195374515
junaid <onlyjunaid2002@gmail.com>
2023-01-09 22:46:42 +0530

    dashboard correction added

--- components/dashboard/Rightbar.tsx
+++ components/dashboard/Rightbar.tsx
@@ -9,9 +9,9 @@ const Rightbar = () => {
               Mathsloyal
             </div>
           </div>
-      <div className="mt-10 mb-4 text-xs text-gray-600">
-        Status of your last question
-      </div>
+          <div className="mt-10 mb-4 text-xs text-gray-600">
+            Status of your last question
+          </div>
           <ol className="relative text-gray-500 border-l border-gray-200 dark:border-gray-700 dark:text-gray-400 ">
             <li className="mb-10 ml-6">
               <span className="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
@@ -30,15 +30,11 @@ const Rightbar = () => {
                 </svg>
               </span>
               <h3 className="font-medium leading-tight">Payment</h3>
-              <p className="text-sm">
-
-Payment is done
-
-              </p>
+              <p className="text-sm">Payment is done</p>
             </li>
             <li className="mb-10 ml-6">
-              <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
-                <svg
+              <span className="absolute flex items-center justify-center w-8 h-8 bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
+                {/* <svg
                   aria-hidden="true"
                   className="w-5 h-5 text-gray-500 dark:text-gray-400"
                   fill="currentColor"
@@ -50,8 +46,22 @@ Payment is done
                     d="M10 2a1 1 0 00-1 1v1a1 1 0 002 0V3a1 1 0 00-1-1zM4 4h3a3 3 0 006 0h3a2 2 0 012 2v9a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2zm2.5 7a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm2.45 4a2.5 2.5 0 10-4.9 0h4.9zM12 9a1 1 0 100 2h3a1 1 0 100-2h-3zm-1 4a1 1 0 011-1h2a1 1 0 110 2h-2a1 1 0 01-1-1z"
                     clipRule="evenodd"
                   />
+                </svg> */}
+                <svg
+                  aria-hidden="true"
+                  className="w-5 h-5 text-green-500 dark:text-green-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
                 </svg>
               </span>
+
               <h3 className="font-medium leading-tight">Finding </h3>
               <p className="text-sm">the best doubt solver </p>
             </li>

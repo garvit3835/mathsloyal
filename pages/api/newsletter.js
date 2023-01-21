1d754a9efa30b3404acc74b81280cd356b24fea1
junaid <onlyjunaid2002@gmail.com>
2023-01-21 22:36:36 +0530

    footer added

---
+++ pages/api/newsletter.js
@@ -0,0 +1,31 @@
+import Newsletter from "../../model/Newsletter";
+import connectDB from "../../middleware/mongoose";
+
+const handler = async (req, res) => {
+    if (req.method === 'POST') {
+
+        const { email } = req.body;
+        if (!email) {
+            return res.status(422).json({ message: 'Invalid email address' });
+        }
+        try {
+            const existingUser = await Newsletter.findOne({ email: email });
+            if (existingUser) {
+                return res.status(422).json({ message: 'you are already close to us' });
+            }
+            const user = new Newsletter({
+                email: email,
+            });
+            await user.save();
+            res.status(201).json({ message: 'Thanks for making us smile' });
+        } catch (error) {
+            res.status(500).json({ message: 'Something went wrong' });
+        }
+
+    }
+    else {
+        res.status(400).json({ message: 'This method is not allowed' });
+    }
+}
+
+export default connectDB(handler);
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,44 +6,44 @@ import Navbar from "../components/home/Navbar";
 import Searchbar from "../components/home/Searchbar";
 import Askdoubt from "../components/user/Askdoubt";
 import Image from "next/image";
+import Footer from "../components/home/Footer";
 
 
 export default function Home() {
   const [showLogin, setShowLogin] = useState('none')
 
   return (
-    <div className="glass-gradient h-screen glass-gradient ">
-      <div>
+    <div className="glass-gradient glass-gradient ">
+      <div className="h-screen ">
+        <Login showLogin={showLogin} setShowLogin={setShowLogin} />
+        <Singup showLogin={showLogin} setShowLogin={setShowLogin} />
+        <Navbar />
 
-      
-      <Login showLogin={showLogin} setShowLogin={setShowLogin} />
-      <Singup showLogin={showLogin} setShowLogin={setShowLogin} />
-      <Navbar />
-
-      {/* <Askdoubt/> */}
-      <div className="lg:flex  h-[679px] relative  justify-around items-center my-auto w-full ">
-        <div className="lg:hidden w-full flex justify-center py-10">
-          <Hero />
-        </div>
-        <div className="flex w-full items-center px-5   justify-around md:pl-7">
-          <div className="md:w-1/2   items-end ">
-            <div className="hidden lg:flex md:px-10 my-10">
-              <Hero />
+        {/* <Askdoubt/> */}
+        <div className="lg:flex  h-[679px] relative  justify-around items-center my-auto w-full ">
+          <div className="lg:hidden w-full flex justify-center py-10">
+            <Hero />
+          </div>
+          <div className="flex w-full items-center px-5   justify-around md:pl-7">
+            <div className="md:w-1/2   items-end ">
+              <div className="hidden lg:flex md:px-10 my-10">
+                <Hero />
+              </div>
+              <Searchbar showLogin={showLogin} setShowLogin={setShowLogin} />
             </div>
-            <Searchbar showLogin={showLogin} setShowLogin={setShowLogin} />
-          </div>
-          <div className="w-2/5 hidden   lg:flex items-end justify-center ">
-            <Image
-              src="/icons/hero.svg"
-              width={600}
-              height={600}
-              className="absolute bottom-0 right-0 w-3/6"
-              alt="a w-full"
-            />
+            <div className="w-2/5 hidden   lg:flex items-end justify-center ">
+              <Image
+                src="/icons/hero.svg"
+                width={600}
+                height={600}
+                className="absolute bottom-0 right-0 w-3/6"
+                alt="a w-full"
+              />
+            </div>
           </div>
         </div>
       </div>
-      </div>
+      <Footer/>
     </div>
   );
 }

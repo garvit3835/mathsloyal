5c96ddac093e4944b38b8c08edd55a3da438ec57
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2023-03-26 10:46:36 +0530

    first commit

--- pages/api/test.js
+++ pages/api/test.js
@@ -9,4 +9,5 @@ export default async function handler(req, res) {
     // let data = await getIssue("641d636d09113c8ef3cc41d9")
     // let data = await createSolution("641d636d09113c8ef3cc41d4", "def.jpg", "this is how it is done", "641d636d09113c8ef3cc41d9") 
     res.status(200).json(data)
+
 }

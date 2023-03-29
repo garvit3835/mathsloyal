5da8b5919d9c604645b89b15e34a6595e41937f8
junaid <onlyjunaid2002@gmail.com>
2023-03-29 19:16:26 +0530

    FSADcxz

--- components/home/Searchbar.jsx
+++ components/home/Searchbar.jsx
@@ -21,7 +21,7 @@ useEffect(() => {
           disabled
           className="w-full h-[45px] xs:h-[57px] xs-max:h-[61px] sm:h-[60px] md:h-[75px] lg:h-[90px] xl:h-[100px]  rounded-[60px] px-3 sm:px-4 md:px-6 text-lg lg:text-xl bg-gray-50 focus:bg-gray-200"
         />
-        <button className="bg-black text-white right-3 hidden sm:block absolute p-3 xl:scale-125  xl:right-6 rounded-full">
+        <button className="bg-black text-white right-3 hidden sm:block absolute p-3 xl:scale-125  xl:right-6 rounded-full" name="search">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             x="0px"
@@ -35,7 +35,7 @@ useEffect(() => {
             <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z" />
           </svg>
         </button>
-        <button className="bg-[#242f40] text-white sm:hidden right-2 xs-max:right-3 absolute p-2 rounded-full   ">
+        <button className="bg-[#242f40] text-white sm:hidden right-2 xs-max:right-3 absolute p-2 rounded-full   " name="search">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             x="0px"

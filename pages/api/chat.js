f99e0083197e0041d5613c7a5a58a59ca84d3984
junaid <onlyjunaid2002@gmail.com>
2023-04-01 13:25:18 +0530

    FSD

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -8,10 +8,14 @@ const handler = async (req, res) => {
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: req.body.text,
+        prompt: getPrompt(req.body.text),
         max_tokens: 900,
         temperature: 0,
     });
     res.status(200).json({ text: response.data.choices[0].text })
+}
+
+const getPrompt = (que)=>{
+    return `suppose you are a best maths tutor for solves very difficult maths doubt in a very easy way my doubt is ${que}`
 }
 export default connectDB(handler);

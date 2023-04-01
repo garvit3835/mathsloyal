395171ef2b23f938a69c937421c43e9edd3a943a
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2023-04-01 17:18:28 +0530

    asf

--- pages/api/payment/verification.js
+++ pages/api/payment/verification.js
@@ -1,20 +1,24 @@
 var crypto = require("crypto");
 
 async function handler(req, res) {
-	let body =
-		req.body.response.razorpay_order_id +
-		"|" +
-		req.body.response.razorpay_payment_id;
+	if (res.method === "POST") {
+		let body =
+			req.body.response.razorpay_order_id +
+			"|" +
+			req.body.response.razorpay_payment_id;
 
-	var expectedSignature = crypto
-		.createHmac("sha256", process.env.RAZORPAY_KEY)
-		.update(body.toString())
-		.digest("hex");
-	console.log("sig received ", req.body.response.razorpay_signature);
-	console.log("sig generated ", expectedSignature);
-	var response = { signatureIsValid: "false" };
-	if (expectedSignature === req.body.response.razorpay_signature)
-		response = { signatureIsValid: "true" };
-	res.send(response);
+		var expectedSignature = crypto
+			.createHmac("sha256", process.env.RAZORPAY_KEY)
+			.update(body.toString())
+			.digest("hex");
+		console.log("sig received ", req.body.response.razorpay_signature);
+		console.log("sig generated ", expectedSignature);
+		var response = { signatureIsValid: "false" };
+		if (expectedSignature === req.body.response.razorpay_signature)
+			response = { signatureIsValid: "true" };
+		res.status(200).json(response);
+	} else {
+		res.status(400).json({ error: "Method not allowed!" });
+	}
 }
 export default handler;
